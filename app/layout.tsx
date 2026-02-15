5f3fb4d2a55168f343194dc399c799c7f36888cf
rassyaabiwafaaa <rassya.abiwafa@gmail.com>
2026-02-15 11:41:50 +0700

    add provider into layout and add interceptor api from axios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// 1. Import Provider-nya
+import Providers from "./QueryClientProvider/QueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* 2. Bungkus children di sini */}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
